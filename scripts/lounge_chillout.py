df4f4d40c20f769c60eb12127cf683ddfd238d4d
Patrick <patrick.hiemsch@gmail.com>
2023-08-03 18:35:36 +0200

    black run

--- scripts/lounge_chillout.py
+++ scripts/lounge_chillout.py
@@ -3,7 +3,9 @@ from traxsource2youtube.youtube import update_top10_playlist, get_authenticated_
 if __name__ == "__main__":
     url = "https://www.traxsource.com/genre/1/lounge-chill-out"
     name = "Lounge / Chill Out - Top 10 on Traxsource"
-    desc = "Playlist with the most latest Lounge / Chill Out - Top 10 Charts on Traxsource"
+    desc = (
+        "Playlist with the most latest Lounge / Chill Out - Top 10 Charts on Traxsource"
+    )
 
     # Get authenticated client
     client = get_authenticated_service()
