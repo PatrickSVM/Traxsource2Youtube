28e0626083d87daa2035e60d56c4f73f7fbffa37
Patrick <patrick.hiemsch@gmail.com>
2023-08-18 16:55:57 +0200

    Only run 4 of the updates at once in script

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 echo "Update all playlists...\n"
-python scripts/afro_latin_brazilian.py
+# python scripts/afro_latin_brazilian.py
 python scripts/deep_house.py
 python scripts/house.py
-python scripts/leftfield.py
-python scripts/lounge_chillout.py
+# python scripts/leftfield.py
+# python scripts/lounge_chillout.py
 python scripts/minimal_deeptech.py
 python scripts/nu_disco_indie_dance.py
 
