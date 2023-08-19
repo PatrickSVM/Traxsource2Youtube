9deccc0ec0060fc4a35dc5f0ff8c24022c1055b3
Patrick <patrick.hiemsch@gmail.com>
2023-08-19 14:11:54 +0200

    Add sleep time for script switch
    * Fixes error that can occure when the API is queried to frequently in time
    * Adds outputlines to better separate between playlist updates

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -1,11 +1,19 @@
 #!/bin/zsh
 
 echo "Update all playlists...\n"
-# python scripts/afro_latin_brazilian.py
+
 python scripts/deep_house.py
+echo "======================================\n\n"
+sleep 3
 python scripts/house.py
-# python scripts/leftfield.py
-# python scripts/lounge_chillout.py
+echo "======================================\n\n"
+sleep 3
 python scripts/minimal_deeptech.py
+echo "======================================\n\n"
+sleep 3
 python scripts/nu_disco_indie_dance.py
 
+
+# python scripts/afro_latin_brazilian.py
+# python scripts/leftfield.py
+# python scripts/lounge_chillout.py
